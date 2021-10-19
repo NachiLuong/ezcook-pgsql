6db809a8c2418c85786da7d063874eae961b19a9
DVietITech <dviet.itech@gmail.com>
2021-10-19 12:58:47 -0700

    update header

--- src/main/java/com/ezcook/controllers/web/WebController.java
+++ src/main/java/com/ezcook/controllers/web/WebController.java
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"/home"})
 public class WebController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws SecurityException, IOException, ServletException {
 
-//        RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
-        RequestDispatcher rd = request.getRequestDispatcher("/common/web/header.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+//        RequestDispatcher rd = request.getRequestDispatcher("/common/web/header.jsp");
         rd.forward(request, response);
     }
 
